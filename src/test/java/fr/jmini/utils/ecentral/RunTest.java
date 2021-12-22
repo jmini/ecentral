22dac57ea56f665d0d8004f2ace3f35e4c32d543
Jeremie Bresson <dev@jmini.fr>
2021-12-22 21:45:17 +0100

    Create BOM for Eclipse release 2021-12
    Fixes #10

--- src/test/java/fr/jmini/utils/ecentral/RunTest.java
+++ src/test/java/fr/jmini/utils/ecentral/RunTest.java
@@ -8,6 +8,15 @@ import org.junit.jupiter.api.Test;
 class RunTest {
 
     @Test
+    void run_4_22() throws Exception {
+        Input input = new Input()
+                .withReleaseName("2021-12")
+                .withReleaseVersion("4.22")
+                .withUpdateSite("https://download.eclipse.org/eclipse/updates/4.22/");
+        new ECentralTask(input).run();
+    }
+
+    @Test
     void run_4_21() throws Exception {
         Input input = new Input()
                 .withReleaseName("2021-09")
