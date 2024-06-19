dfb8ed9dd2a2543abdcb66df9c75aa1193b2fe5e
arnaud.mergey@semarchy.com <arnaud.mergey@semarchy.com>
2024-06-19 03:50:17 +0200

    update eclipse bom
    + update gradle wrapper
    [Gradle Release Plugin] - new version commit:  '2.0.2-SNAPSHOT'.

--- src/test/java/fr/jmini/utils/ecentral/RunTest.java
+++ src/test/java/fr/jmini/utils/ecentral/RunTest.java
@@ -11,6 +11,35 @@ import org.junit.jupiter.api.Test;
  * This test class run the code for different update sites.
  */
 class RunTest {
+    @Test
+    void run_2024_06() throws Exception {
+        Input input = new Input()
+                .withReleaseName("2024-06")
+                .withArtifactId("eclipse-full-dependencies");
+        ECentralTask task = new ECentralTask(input);
+        task.run();
+        String mavenArtifacts = Files.readString(task.getMavenArtifactsFile(), StandardCharsets.UTF_8);
+
+        assertThatJson(mavenArtifacts).isArray()
+                .size()
+                .isEqualTo(task.parseBndOutput()
+                        .size());
+    }
+
+    @Test
+    void run_2024_03() throws Exception {
+        Input input = new Input()
+                .withReleaseName("2024-03")
+                .withArtifactId("eclipse-full-dependencies");
+        ECentralTask task = new ECentralTask(input);
+        task.run();
+        String mavenArtifacts = Files.readString(task.getMavenArtifactsFile(), StandardCharsets.UTF_8);
+
+        assertThatJson(mavenArtifacts).isArray()
+                .size()
+                .isEqualTo(task.parseBndOutput()
+                        .size());
+    }
 
     @Test
     void run_2023_12() throws Exception {
