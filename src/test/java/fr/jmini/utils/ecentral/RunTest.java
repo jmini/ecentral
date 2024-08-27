8e61fd3d076f0e0fd4c25671a121cd1fd85928b9
Jérémie Bresson <dev@jmini.fr>
2024-08-27 16:19:13 +0000

    Create BOM for Eclipse release 2023-09

--- src/test/java/fr/jmini/utils/ecentral/RunTest.java
+++ src/test/java/fr/jmini/utils/ecentral/RunTest.java
@@ -8,6 +8,15 @@ import org.junit.jupiter.api.Test;
 class RunTest {
 
     @Test
+    void run_4_29() throws Exception {
+        Input input = new Input()
+                .withReleaseName("2023-09")
+                .withReleaseVersion("4.29")
+                .withUpdateSite("https://download.eclipse.org/eclipse/updates/4.29/");
+        new ECentralTask(input).run();
+    }
+
+    @Test
     void run_4_28() throws Exception {
         Input input = new Input()
                 .withReleaseName("2023-06")
